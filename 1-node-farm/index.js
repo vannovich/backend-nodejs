b33e43c54ff2670f0682543f4329bf11ec668e55
vannovich <awontuvannovich@gmail.com>
2026-03-12 14:45:39 +0100

    Intro to Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ///////////////
 //files
@@ -34,9 +35,18 @@ const http = require("http");
 ////////////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-    // console.log(req);
-    // console.log(res);
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/overview") {
+    res.end("THis is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is an incredible product");
+  } else {
+    res.writeHead(404, {
+        'Content-type': 'text/html'
+    })
+    res.end("Page Not Found");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
